52530c19fa40a8a7b0c6f2751c6898c314a51085
direct66612 <direct66612@gmail.com>
2023-12-05 02:16:10 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
-const Layout = lazy(() => import('Layout/Layout'));
-const HomePage = lazy(() => import('pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
+const Layout = lazy(() => import('../Layout/Layout'));
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() =>
-  import('pages/MovieDetailsPage/MovieDetailsPage')
+  import('../pages/MovieDetailsPage/MovieDetailsPage')
 );
-const Cast = lazy(() => import('components/Cast/Cast'));
-const Reviews = lazy(() => import('components/Reviews/Reviews'));
+const Cast = lazy(() => import('../components/Cast/Cast'));
+const Reviews = lazy(() => import('../components/Reviews/Reviews'));
 const App = () => {
   return (
     <Suspense fallback={<p>Loading...</p>}>
